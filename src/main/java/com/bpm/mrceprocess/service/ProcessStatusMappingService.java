e02b5a75af84194510fadcbcb1d556fe914e93ad
ANHNT310 <anhnt.310.hcm@gmail.com>
2025-08-26 10:16:14 +0700

    commit

--- src/main/java/com/bpm/mrceprocess/service/ProcessStatusMappingService.java
+++
@@ -1,7 +0,0 @@
-package com.bpm.mrceprocess.service;
-
-import com.bpm.mrceprocess.common.dtos.ProcessStatusMappingDTO;
-import com.bpm.mrceprocess.persistence.entity.ProcessStatusMapping;
-
-public interface ProcessStatusMappingService extends CrudService<ProcessStatusMapping, ProcessStatusMappingDTO, String> {
-}
